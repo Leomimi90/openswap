9c7ba521f5c290c83e927ee38d580f2a3d13930e
Leomimi90 <fombut@gmail.com>
2021-09-10 16:13:02 +0100

    added and purchase endpoints which are transactions

---
+++ Controllers/TransactionController.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  sale: (req, res, next) => {
+    res.json({ success: true, data: [], message: "Sale successfully made" })
+    next()
+  },
+  purchase: (req, res, next) => {
+    res.json({ success: true, data: [], message: "Successful Purchase " })
+    next()
+  }
+}
--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,17 @@ var router = express.Router();
 var PublicController = require('../Controllers/PublicController');
 var UserController = require('../Controllers/UserController');
 var TradeController = require('../Controllers/TradeController');
+var TransactionController = require('../Controllers/TransactionController');
 
 router.get('/', PublicController.index);
 
 /* User Routes */
 router.get('/users', UserController.index);
 router.get('/users/register', UserController.register);
+
+/* Transaction Routes*/
+router.get('/transaction/sale/:id', TransactionController.sale)
+router.get('/transaction/purchase/:id', TransactionController.purchase)
 
 /* Trade Routes */
 router.get('/trades', TradeController.index);
