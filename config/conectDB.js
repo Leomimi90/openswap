617fb1c183721895ce4cf52240b474be1e91388f
Uchenna David <uchennadavid153@gmail.com>
2021-09-14 01:05:31 +0100

    moved from type orm to sequelize
    Signed-off-by: Uchenna David <uchennadavid153@gmail.com>

---
+++ config/conectDB.js
@@ -0,0 +1,19 @@
+const {Sequelize} = require('sequelize')
+require('dotenv').config()
+
+
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASS, {
+    host:'',
+    dialect:'mysql'
+})
+
+let connection = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log('connection has been established successfuly');
+    } catch (error) {
+        console.error('Unable to coonect to database', error);
+    }
+}
+
+connection();
---
+++ src/migrations/20210913223924-create-user.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique:"unique_tag"
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }, 
+      
+    },
+      {
+        uniqueKeys: {
+          unique_tag: {
+            customIndex: true,
+            fields: ["email"]
+          }
+        }
+      }
+    
+    );
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
